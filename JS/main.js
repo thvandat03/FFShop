349704b4ef32852ae8ed30dddf4f6870fbb81574
thvandat03 <thvandat03@gmail.com>
2022-12-04 00:16:48 +0700

    UP

--- JS/main.js
+++ JS/main.js
@@ -18,3 +18,16 @@ setTimeout(() => {
     
 }, 1000);
 
+
+
+let swiper = new Swiper(".mySwiper", {
+  lazy: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+ });
